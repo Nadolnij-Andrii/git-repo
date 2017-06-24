dd3ba2b213bf464833f4f1bb88e1e74fdd3d5868
slayerhp <andnad6@gmail.com>
2017-06-24 14:01:17 +0300

    1 add

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hellow W");
+            Console.WriteLine("What`s up?");
         }
     }
 }
